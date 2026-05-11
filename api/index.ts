7fac01585d3dbd9974a69d937461f55dc73a1e19
mail.bernand@gmail.com <mail.bernand@gmail.com>
2026-05-11 13:08:09 +0700

    fix: Vercel

--- api/index.ts
+++ api/index.ts
@@ -1,12 +1,28 @@
-import { createRequestHandler } from "@tanstack/react-start/server";
-import { getRouter } from "../src/router";
+import type { VercelRequest, VercelResponse } from "@vercel/node";
 
-const handler = createRequestHandler({
-  getRouter,
-});
+export default async function (req: VercelRequest, res: VercelResponse) {
+  const url = new URL(req.url, `http://${req.headers.host}`);
+  
+  // Import dan jalankan TanStack Start handler
+  // @ts-ignore
+  const { fetch } = await import("../dist/server/index.js");
+  
+  const request = new Request(url.toString(), {
+    method: req.method,
+    headers: req.headers as HeadersInit,
+    body: ["POST", "PUT", "PATCH"].includes(req.method) ? JSON.stringify(req.body) : undefined,
+  });
 
-export default handler;
+  const response = await fetch(request, {}, {});
+  
+  res.status(response.status);
+  response.headers.forEach((value, key) => {
+    res.setHeader(key, value);
+  });
+  
+  res.send(await response.text());
+}
 
 export const config = {
-  runtime: "nodejs22.x",
+  runtime: "edge",
 };
